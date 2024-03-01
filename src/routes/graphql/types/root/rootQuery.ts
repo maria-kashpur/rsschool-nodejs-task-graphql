bf9a0529e7bab629a408b3c5779500484b76a832
maria-kashpur <kashpur.maria@icloud.com>
2024-03-01 13:47:49 +0300

    fix: update rootQuery

--- src/routes/graphql/types/root/rootQuery.ts
+++ src/routes/graphql/types/root/rootQuery.ts
@@ -1,16 +1,56 @@
-import { GraphQLObjectType } from 'graphql';
+import { GraphQLNonNull, GraphQLObjectType, GraphQLString } from 'graphql';
+import { UserType, UsersType } from '../user.js';
+import { UUIDType } from '../uuid.js';
+import { PostType, PostsType } from '../post.js';
+import { ProfileType, ProfilesType } from '../profile.js';
+import { MemberType, MemberTypeId, MemberTypesType } from '../member.js';
+
+const id = { type: new GraphQLNonNull(UUIDType) };
+const title = { type: new GraphQLNonNull(GraphQLString) };
+const content = { type: GraphQLString };
+const authorId = { type: new GraphQLNonNull(UUIDType) };
 
 const rootQuery = new GraphQLObjectType({
   name: 'RootQueryType',
   fields: {
-    // user: {},
-    // users: {},
-    // post: {},
-    // posts: {},
-    // profile: {},
-    // profiles: {},
-    // memberType: {},
-    // memberTypes: {},
+    user: {
+      type: UserType,
+      args: {
+        id,
+      },
+    },
+    users: {
+      type: UsersType,
+    },
+    post: {
+      type: PostType,
+      args: {
+        id,
+      },
+    },
+    posts: {
+      type: PostsType,
+    },
+    profile: {
+      type: ProfileType,
+      args: {
+        id,
+      },
+    },
+    profiles: {
+      type: ProfilesType,
+    },
+    memberType: {
+      type: MemberType,
+      args: {
+        id: {
+          type: MemberTypeId,
+        },
+      },
+    },
+    memberTypes: {
+      type: MemberTypesType,
+    },
   },
 });
 
